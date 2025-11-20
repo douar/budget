b2245f6dd183e09914eac03df3431e5499ca5920
douarmoua <mouadouarzong@gmail.com>
2025-11-19 18:58:16 -0600

    ng g c expense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExpenseComponent } from './components/expense/expense.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseComponent
   ],
   imports: [
     BrowserModule
