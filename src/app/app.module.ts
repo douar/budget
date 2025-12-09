036b22349037b449603c46f5633b5a4c12f55b32
douarmoua <mouadouarzong@gmail.com>
2025-12-09 00:18:12 -0600

    ng g create-new page, render components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ExpenseComponent } from './components/create/expense/expense.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { BudgetComponent } from './components/create/budget/budget.component';
+import { CreateNewComponent } from './components/create/create-new/create-new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExpenseComponent,
-    BudgetComponent
+    BudgetComponent,
+    CreateNewComponent
   ],
   imports: [
     BrowserModule,
