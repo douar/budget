fb8dfdbebbb2df1f470371c360eab16c7ce9ea0d
douarmoua <mouadouarzong@gmail.com>
2025-12-15 18:03:45 -0600

    ng g c income, conditionally render income

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { ExpenseComponent } from './components/create/expense/expense.component'
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { BudgetComponent } from './components/create/budget/budget.component';
 import { CreateNewComponent } from './components/create/create-new/create-new.component';
+import { IncomeComponent } from './components/create/income/income.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExpenseComponent,
     BudgetComponent,
-    CreateNewComponent
+    CreateNewComponent,
+    IncomeComponent
   ],
   imports: [
     BrowserModule,
